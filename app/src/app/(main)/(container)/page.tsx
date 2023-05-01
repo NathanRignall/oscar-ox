85e93564a677fb6527869d536bcf484f3c1f5249
Nathan Rignall <rignallnathan@gmail.com>
2023-05-01 19:35:52 +0100

    Set the homepage to load dynamic data

--- app/src/app/(main)/(container)/page.tsx
+++ app/src/app/(main)/(container)/page.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { createServerClient } from "@/lib/supabase-server";
+import { getArray, getSingle } from "@/lib/supabase-type-convert";
 import clsx from "clsx";
 
 // event component props
@@ -6,6 +9,7 @@ type CalenderEventProps = {
   time: string;
   location: string;
   variant: "red" | "orange" | "yellow" | "green" | "blue" | "pink" | "purple";
+  href: string;
 };
 
 // event component
@@ -14,68 +18,153 @@ const CalenderEvent = ({
   time,
   location,
   variant,
+  href,
 }: CalenderEventProps) => {
   return (
-    <div
-      className={clsx(
-        variant == "red" && "bg-red-100 border-red-900",
-        variant == "orange" && "bg-orange-100 border-orange-900",
-        variant == "yellow" && "bg-yellow-100 border-yellow-900",
-        variant == "green" && "bg-green-100 border-green-900",
-        variant == "blue" && "bg-blue-100 border-blue-900",
-        variant == "pink" && "bg-pink-100 border-pink-900",
-        variant == "purple" && "bg-purple-100 border-purple-900",
-        "rounded-lg border-l-[3px]  px-3 py-1 mb-3"
-      )}
-    >
-      <p
+    <Link href={href}>
+      <div
         className={clsx(
-          variant == "red" && "text-red-900",
-          variant == "orange" && "text-orange-900",
-          variant == "yellow" && "text-yellow-900",
-          variant == "green" && "text-green-900",
-          variant == "blue" && "text-blue-900",
-          variant == "pink" && "text-pink-900",
-          variant == "purple" && "text-purple-900",
-          "text-xs font-bold"
+          variant == "red" && "bg-red-100 border-red-900",
+          variant == "orange" && "bg-orange-100 border-orange-900",
+          variant == "yellow" && "bg-yellow-100 border-yellow-900",
+          variant == "green" && "bg-green-100 border-green-900",
+          variant == "blue" && "bg-blue-100 border-blue-900",
+          variant == "pink" && "bg-pink-100 border-pink-900",
+          variant == "purple" && "bg-purple-100 border-purple-900",
+          "rounded-lg border-l-[3px]  px-3 py-1 mb-3"
         )}
       >
-        {time}
-      </p>
-      <p
-        className={clsx(
-          variant == "red" && "text-red-900",
-          variant == "orange" && "text-orange-900",
-          variant == "yellow" && "text-yellow-900",
-          variant == "green" && "text-green-900",
-          variant == "blue" && "text-blue-900",
-          variant == "pink" && "text-pink-900",
-          variant == "purple" && "text-purple-900",
-          " text-sm font-medium"
-        )}
-      >
-        {title}
-      </p>
-      <p
-        className={clsx(
-          variant == "red" && "text-red-800",
-          variant == "orange" && "text-orange-800",
-          variant == "yellow" && "text-yellow-800",
-          variant == "green" && "text-green-800",
-          variant == "blue" && "text-blue-800",
-          variant == "pink" && "text-pink-800",
-          variant == "purple" && "text-purple-800",
-          "text-xs font-base"
-        )}
-      >
-        {location}
-      </p>
-    </div>
+        <p
+          className={clsx(
+            variant == "red" && "text-red-900",
+            variant == "orange" && "text-orange-900",
+            variant == "yellow" && "text-yellow-900",
+            variant == "green" && "text-green-900",
+            variant == "blue" && "text-blue-900",
+            variant == "pink" && "text-pink-900",
+            variant == "purple" && "text-purple-900",
+            "text-xs font-bold"
+          )}
+        >
+          {time}
+        </p>
+        <p
+          className={clsx(
+            variant == "red" && "text-red-900",
+            variant == "orange" && "text-orange-900",
+            variant == "yellow" && "text-yellow-900",
+            variant == "green" && "text-green-900",
+            variant == "blue" && "text-blue-900",
+            variant == "pink" && "text-pink-900",
+            variant == "purple" && "text-purple-900",
+            " text-sm font-medium"
+          )}
+        >
+          {title}
+        </p>
+        <p
+          className={clsx(
+            variant == "red" && "text-red-800",
+            variant == "orange" && "text-orange-800",
+            variant == "yellow" && "text-yellow-800",
+            variant == "green" && "text-green-800",
+            variant == "blue" && "text-blue-800",
+            variant == "pink" && "text-pink-800",
+            variant == "purple" && "text-purple-800",
+            "text-xs font-base"
+          )}
+        >
+          {location}
+        </p>
+      </div>
+    </Link>
   );
 };
 
 // Page
-export default function Home() {
+export default async function Home() {
+  const supabase = createServerClient();
+
+  // get todays date at midnight
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const todayString = today.toISOString();
+
+  // get date 7 days from now at midnight
+  const nextWeek = new Date();
+  nextWeek.setDate(nextWeek.getDate() + 7);
+  nextWeek.setHours(0, 0, 0, 0);
+  const nextWeekString = nextWeek.toISOString();
+
+  // get the events
+  const { data: _events } = await supabase
+    .from("events")
+    .select(
+      `
+      id,
+      production: productions (
+        id,
+        title,
+        company_id
+      ),
+      venue: venues (
+        id,
+        title
+      ),
+      start_time,
+      end_time
+    `
+    )
+    .gte("start_time", todayString)
+    .lte("start_time", nextWeekString);
+
+  const events = getArray(_events).map((event) => {
+    return {
+      id: event.id,
+      production: getSingle(event.production),
+      venue: getSingle(event.venue),
+      start_time: event.start_time,
+      end_time: event.end_time,
+    };
+  });
+
+  // get a list of all the productions with no duplicates
+  const productions = events
+    .map((event) => event.production)
+    .filter(
+      (production, index, self) =>
+        self.findIndex((p) => p.id === production.id) === index
+    );
+
+  // give a colour to each production
+  const productionColours = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "pink",
+    "purple",
+  ];
+  const productionColoursMap = new Map();
+  productions.forEach((production, index) => {
+    productionColoursMap.set(production.id, productionColours[index]);
+  });
+
+  // form a list of events separated by day
+  const days = [];
+  for (let i = 1; i <= 7; i++) {
+    const day = new Date();
+    day.setDate(today.getDate() + i);
+    day.setHours(0, 0, 0, 0);
+
+    const dayEvents = events.filter(
+      (event) =>
+        event.start_time.slice(0, 10) === day.toISOString().slice(0, 10)
+    );
+    days.push(dayEvents);
+  }
+
   return (
     <>
       <header className="mb-8">
@@ -173,141 +262,51 @@ export default function Home() {
           </form>
         </div>
 
-        <div className="bg-white rounded-lg border-2 border-slate-200 w-full">
+        <div className="bg-white rounded-lg border-2 border-slate-200 w-full overflow-hidden">
           <div className="grid grid-cols-1 lg:grid-cols-7">
-            <div>
-              <div className="bg-slate-50 border-b-2 border-slate-200 px-4 py-4">
-                <p className="text-slate-500 text-xs font-semibold">SUN</p>
-                <p className="text-slate-900 text-sm font-semibold">JAN 22</p>
-              </div>
-              <div className="px-4 py-4">
-                <CalenderEvent
-                  title="Mary Poppins"
-                  time="17:00"
-                  location="The Playhouse"
-                  variant="red"
-                />
-                <CalenderEvent
-                  title="American in Paris"
-                  time="19:30"
-                  location="Oxford Theatre"
-                  variant="green"
-                />
-                <CalenderEvent
-                  title="The thwarting of Baron Bolligrew"
-                  time="19:30"
-                  location="The Auditorium"
-                  variant="orange"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="bg-slate-50 border-b-2 border-slate-200 px-4 py-4">
-                <p className="text-slate-500 text-xs font-semibold">SUN</p>
-                <p className="text-slate-900 text-sm font-semibold">JAN 22</p>
-              </div>
-              <div className="px-4 py-4">
-                <CalenderEvent
-                  title="Matilda"
-                  time="18:00"
-                  location="The Playhouse"
-                  variant="purple"
-                />
-                <CalenderEvent
-                  title="American in Paris"
-                  time="19:30"
-                  location="Oxford Theatre"
-                  variant="green"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="bg-slate-50 border-b-2 border-slate-200 px-4 py-4">
-                <p className="text-slate-500 text-xs font-semibold">SUN</p>
-                <p className="text-slate-900 text-sm font-semibold">JAN 22</p>
-              </div>
-              <div className="px-4 py-4">
-                <CalenderEvent
-                  title="Matilda"
-                  time="18:00"
-                  location="The Playhouse"
-                  variant="purple"
-                />
-                <CalenderEvent
-                  title="The thwarting of Baron Bolligrew"
-                  time="19:30"
-                  location="The Auditorium"
-                  variant="orange"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="bg-slate-50 border-b-2 border-slate-200 px-4 py-4">
-                <p className="text-slate-500 text-xs font-semibold">SUN</p>
-                <p className="text-slate-900 text-sm font-semibold">JAN 22</p>
-              </div>
-              <div className="px-4 py-4"></div>
-            </div>
-            <div>
-              <div className="bg-slate-50 border-b-2 border-slate-200 px-4 py-4">
-                <p className="text-slate-500 text-xs font-semibold">SUN</p>
-                <p className="text-slate-900 text-sm font-semibold">JAN 22</p>
-              </div>
-              <div className="px-4 py-4">
-                <CalenderEvent
-                  title="Mary Poppins"
-                  time="17:00"
-                  location="The Playhouse"
-                  variant="red"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="bg-slate-50 border-b-2 border-slate-200 px-4 py-4">
-                <p className="text-slate-500 text-xs font-semibold">SUN</p>
-                <p className="text-slate-900 text-sm font-semibold">JAN 22</p>
-              </div>
-              <div className="px-4 py-4">
-                <CalenderEvent
-                  title="Mary Poppins"
-                  time="17:00"
-                  location="The Playhouse"
-                  variant="red"
-                />
-                <CalenderEvent
-                  title="American in Paris"
-                  time="19:30"
-                  location="Oxford Theatre"
-                  variant="green"
-                />
-                <CalenderEvent
-                  title="Joseph and the Amazing Technicolor Dreamcoat"
-                  time="19:30"
-                  location="The Auditorium"
-                  variant="pink"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="bg-slate-50 border-b-2 border-slate-200 px-4 py-4">
-                <p className="text-slate-500 text-xs font-semibold">SUN</p>
-                <p className="text-slate-900 text-sm font-semibold">JAN 22</p>
-              </div>
-              <div className="px-4 py-4">
-                <CalenderEvent
-                  title="Mary Poppins"
-                  time="17:00"
-                  location="The Playhouse"
-                  variant="red"
-                />
-                <CalenderEvent
-                  title="American in Paris"
-                  time="19:30"
-                  location="Oxford Theatre"
-                  variant="green"
-                />
-              </div>
-            </div>
+            {days.map((dayEvents, index) => {
+              const day = new Date();
+              day.setDate(today.getDate() + index);
+              day.setHours(0, 0, 0, 0);
+
+              return (
+                <div key={index}>
+                  <div className="bg-slate-50 border-b-2 border-slate-200 px-4 py-4">
+                    <p className="text-slate-500 text-xs font-semibold uppercase">
+                      {day.toLocaleDateString("en-GB", {
+                        weekday: "short",
+                      })}
+                    </p>
+                    <p className="text-slate-900 text-sm font-semibold uppercase">
+                      {day.toLocaleDateString("en-GB", {
+                        day: "numeric",
+                        month: "short",
+                      })}
+                    </p>
+                  </div>
+                  <div className="px-4 py-4">
+                    {dayEvents.map((event) => {
+                      const time = new Date(event.start_time);
+                      return (
+                        <CalenderEvent
+                          key={event.id}
+                          title={event.production.title}
+                          time={time.toLocaleTimeString("en-GB", {
+                            hour: "2-digit",
+                            minute: "2-digit",
+                          })}
+                          location={event.venue.title}
+                          variant={productionColoursMap.get(
+                            event.production.id
+                          )}
+                          href={`/companies/${event.production.company_id}`}
+                        />
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </main>
