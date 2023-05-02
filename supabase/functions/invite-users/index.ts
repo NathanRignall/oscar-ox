93d7f9172c8800e88bc3c0280f90d5994a785a68
Nathan Rignall <rignallnathan@gmail.com>
2023-05-02 08:36:18 +0100

    Add support for user invites

---
+++ supabase/functions/invite-users/index.ts
@@ -0,0 +1,48 @@
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { Database } from "../db-types.ts";
+
+type User = {
+  email: string;
+  name: string;
+};
+
+interface Payload {
+  users: User[];
+}
+
+serve(async (req) => {
+  const payload: Payload = await req.json();
+
+  // take an array of email and names and use them to invite users to the platform
+
+  try {
+    // connect to supabase
+    const supabaseClient = createClient<Database>(
+      Deno.env.get("SUPABASE_URL") as string,
+      Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") as string
+    );
+
+    // invite users
+    Promise.all(
+      payload.users.map(async (user) => {
+        const { error } = await supabaseClient.auth.admin.inviteUserByEmail(
+          user.email,
+          {
+            data: {
+              name: user.name,
+            },
+            redirectTo : "https://ox.nlr.app/auth/invite",
+          }
+        );
+
+        if (error) throw new Error(error.message);
+      })
+    );
+
+    return new Response("done");
+  } catch (error) {
+    console.error(error);
+    return new Response(error.message, { status: 500 });
+  }
+});
