e74a83df15224712b743acd8679f3b9595070a91
Nathan Rignall <rignallnathan@gmail.com>
2023-05-05 16:53:49 +0100

    Fix custom page slug

--- app/src/app/(main)/(full)/companies/[companySlug]/(loading)/[pageSlug]/page.tsx
+++ app/src/app/(main)/(full)/companies/[companySlug]/(loading)/[pageSlug]/page.tsx
@@ -16,7 +16,7 @@ const Themes = {
 export default async function Page({
   params,
 }: {
-  params: { companyId: string; pageSlug: string };
+  params: { companySlug: string; pageSlug: string };
 }) {
   const supabase = createServerClient();
 
@@ -24,10 +24,11 @@ export default async function Page({
     .from("companies")
     .select(
       `
+        id,
         theme
       `
     )
-    .match({ id: params.companyId })
+    .match({ slug: params.companySlug })
     .single();
 
   if (!company) notFound();
@@ -40,18 +41,18 @@ export default async function Page({
         title
         `
     )
-    .match({ company_id: params.companyId, slug: params.pageSlug })
+    .match({ company_id: params.companySlug, slug: params.pageSlug })
     .single();
 
   if (!page) notFound();
 
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/media/companies/${params.companyId}/pages/${page.id}.mdx`
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/media/companies/${company.id}/pages/${page.id}.mdx`
   );
 
   const markdown = await res.text();
 
   const Page = Themes[company.theme];
 
-  return <Page source={markdown} companyId={params.companyId} />;
+  return <Page source={markdown} companyId={params.companySlug} />;
 }
