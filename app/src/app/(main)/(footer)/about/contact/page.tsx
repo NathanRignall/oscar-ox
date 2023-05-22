28eee11d82a3501bb524a4f5b33ff963e88c6f43
Nathan Rignall <rignallnathan@gmail.com>
2023-05-22 08:35:33 +0100

    Add template contact page

--- app/src/app/(main)/(footer)/about/contact/page.tsx
+++ app/src/app/(main)/(footer)/about/contact/page.tsx
@@ -13,8 +13,19 @@ export default async function Roles() {
       <main className="max-w-3xl mx-auto">
         <article className="mt-8">
           <h2 className="font-bold text-3xl mt-3 mb-1 text-slate-900 dark:text-white">
-            TODO
+            Comming Soon...
           </h2>
+
+          <p className="text-slate-600 dark:text-slate-300">
+            This page is under construction. Please check back later. For any urgent
+            matters, please contact us at{" "}
+            <a
+              href="mailto:dev@ox.nlr.app"
+              className="underline text-slate-900 dark:text-white"
+            >
+              dev@ox.nlr.app
+            </a>
+          </p>
         </article>
       </main>
     </>
