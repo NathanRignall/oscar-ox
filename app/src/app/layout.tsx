2aae3ac94beb2cea14bfe0bf9ce497c533b4d236
Nathan Rignall <rignallnathan@gmail.com>
2024-02-12 10:36:09 +0000

    Add tracking

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { createServerClient } from "@/lib/supabase-server";
 import { SupabaseListener, SupabaseProvider } from "@/components/client";
 import { Logger } from "./Logger";
+import Script from "next/script";
 
 // do not cache this layout
 export const revalidate = 0;
@@ -28,6 +29,8 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head />
+
+      <Script async src="https://analytics.nlr.app/script.js" data-website-id="5f46a410-0a80-4d6d-8bb0-8b5712fa04ee"/>
 
       <body className="bg-slate-50 dark:bg-slate-900 h-screen flex flex-col">
         <SupabaseProvider session={session}>
